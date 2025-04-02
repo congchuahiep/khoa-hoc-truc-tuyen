f3d8280ce288b50451b10089910617fa642a2bb3
Trần Hoàng Hiệp <congchuahiep@gmail.com>
2025-04-02 23:32:04 +0700

    Remove unused IDE configuration files and add utility functions for Cloudinary image handling in admin interface

--- courses/admin.py
+++ courses/admin.py
@@ -1,58 +1,111 @@
 from django.contrib import admin
 from django.db.models import Count
 from django.template.response import TemplateResponse
-from django.utils.safestring import mark_safe
 
-from courses.models import Category, Course, Lesson, Tag, Comment
+from courses.models import Category, Course, Lesson, Tag, Comment, User
+from courses.utils import get_cloudinary_image
 from django.urls import path
 
 from django import forms
 from ckeditor_uploader.widgets import CKEditorUploadingWidget
 
+from django.utils.safestring import mark_safe
+
 
 class LessonForm(forms.ModelForm):
     content = forms.CharField(widget=CKEditorUploadingWidget)
+
     class Meta:
         model = Lesson
-        fields = '__all__'
+        fields = "__all__"
+
+
+class MyCourseAdmin(admin.ModelAdmin):
+    list_display = ["id", "subject", "description"]
+    list_editable = ["subject"]
+    readonly_fields = ["image_view"]
+
+    def image_view(self, course):
+        if course:
+            return get_cloudinary_image(
+                course.image.public_id, transformations={"width": 200}
+            )
 
 
 class MyLessonAdmin(admin.ModelAdmin):
-    list_display = ['id', 'subject', 'active', 'created_date']
-    search_fields = ['subject']
-    list_filter = ['id', 'created_date']
-    list_editable = ['subject']
-    readonly_fields = ['image_view']
+    list_display = ["id", "subject", "active", "created_date"]
+    search_fields = ["subject"]
+    list_filter = ["id", "created_date"]
+    list_editable = ["subject"]
+    readonly_fields = ["image_view"]
     form = LessonForm
 
     def image_view(self, lesson):
         if lesson:
-            return mark_safe(f"<img src='/static/{lesson.image.name}' width='200' />")
+            return get_cloudinary_image(
+                lesson.image.public_id, transformations={"width": 200}
+            )
 
     class Media:
-        css = {
-            'all': ('/static/css/styles.css', )
-        }
+        css = {"all": ("/static/css/styles.css",)}
+
+
+class MyUserAdmin(admin.ModelAdmin):
+    list_display = ["status_display", "username", "email"]
+    search_fields = ["username", "email"]
+    list_filter = ["is_active", "date_joined"]
+
+    fieldsets = [
+        ("Thông tin cá nhân", {"fields": ["status_display", "username", "email", "avatar_view"]}),
+        (
+            "Quyền truy cập",
+            {
+                "description": "Thiết lập quyền truy cập của người dùng",
+                "classes": ["collapse"],
+                "fields": ["user_permissions", "is_staff", "is_superuser"],
+            },
+        ),
+    ]
+
+    # list_editable = ['is_staff']
+    readonly_fields = ["avatar_view", "status_display"]
+    filter_horizontal = ["user_permissions"]
+
+    def avatar_view(self, user):
+        if user:
+            return get_cloudinary_image(
+                user.avatar.public_id, transformations={"width": 200}
+            )
+
+    def status_display(self, user):
+        """Hiển thị trạng thái dưới dạng biểu tượng màu."""
+        color = "green" if user.is_active else "red"
+        return mark_safe(f'<span style="color: {color};">●</span>')
+
+    status_display.short_description = "Trạng thái"
 
 
 class MyAdminSite(admin.AdminSite):
-    site_header = 'OU eCourse Online'
+    site_header = "OU eCourse Online"
 
     def get_urls(self):
-        return [path('course-stats/', self.course_stats),] + super().get_urls()
+        return [
+            path("course-stats/", self.course_stats),
+        ] + super().get_urls()
 
     def course_stats(self, request):
-        stats = Category.objects.annotate(course_count=Count('course__id')).values('id', 'name', 'course_count')
+        stats = Category.objects.annotate(course_count=Count("course__id")).values(
+            "id", "name", "course_count"
+        )
 
-        return TemplateResponse(request, 'admin/stats.html', {
-            'stats': stats
-        })
+        return TemplateResponse(request, "admin/stats.html", {"stats": stats})
 
 
-admin_site = MyAdminSite(name='eCourse')
+admin_site = MyAdminSite(name="eCourse")
 
 admin_site.register(Category)
-admin_site.register(Course)
+admin_site.register(Course, MyCourseAdmin)
 admin_site.register(Lesson, MyLessonAdmin)
 admin_site.register(Tag)
 admin_site.register(Comment)
+admin_site.register(User, MyUserAdmin)
--- courses/views.py
+++ courses/views.py
@@ -5,8 +5,14 @@ from rest_framework.decorators import action
 
 from courses import paginators
 from .models import Category, Course, Lesson, User
-from .serializers import CategorySerializer, CourseSerializer, LessonSerializer, LessonDetailSerializer, \
-    CommentSerializer, UserSerializer
+from .serializers import (
+    CategorySerializer,
+    CourseSerializer,
+    LessonSerializer,
+    LessonDetailSerializer,
+    CommentSerializer,
+    UserSerializer,
+)
 
 
 # Use ListAPIView for listing view
@@ -16,6 +22,7 @@ from .serializers import CategorySerializer, CourseSerializer, LessonSerializer,
 class CategoryViewSet(viewsets.ViewSet, generics.ListAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
 
 class CourseViewSet(viewsets.ViewSet, generics.ListAPIView):
     queryset = Course.objects.all()
@@ -27,77 +34,93 @@ class CourseViewSet(viewsets.ViewSet, generics.ListAPIView):
     def get_queryset(self):
         query = self.queryset
 
-        q = self.request.query_params.get('q')
+        q = self.request.query_params.get("q")
         if q:
             query = query.filter(subject__icontains=q)
 
-        cate_id = self.request.query_params.get('category_id')
+        cate_id = self.request.query_params.get("category_id")
         if cate_id:
             query = query.filter(category_id=cate_id)
 
         return query
 
     # make a detail child url like /courses/{course_id}/lessons/
-    @action(methods=['get'], detail=True, url_name='lessons')
+    @action(methods=["get"], detail=True, url_name="lessons")
     def get_lessons(self, request, pk):
         lesson = self.get_object().lesson_set.filter(active=True)
-        return Response(LessonSerializer(lesson, many=True).data, status=rest_framework.status.HTTP_200_OK)
+        return Response(
+            LessonSerializer(lesson, many=True).data,
+            status=rest_framework.status.HTTP_200_OK,
+        )
 
 
 class LessonViewSet(viewsets.ViewSet, generics.RetrieveAPIView):
-    queryset = Lesson.objects.prefetch_related('tags').filter(active=True)
+    queryset = Lesson.objects.prefetch_related("tags").filter(active=True)
     serializer_class = LessonDetailSerializer
-    
+
     # custom permission
     def get_permissions(self):
-        '''
+        """
         only user have IsAuthenticated permission can be access to:
         POST:/lesson/{lesson_id}/comments/ action
-        '''
-        if self.action in ['get_comments'] and self.request.method.__eq__('POST'):
+        """
+        if self.action in ["get_comments"] and self.request.method.__eq__("POST"):
             return [permissions.IsAuthenticated()]
-        
+
         # other action can be used by anyone
         return [permissions.AllowAny()]
 
     # make a detail child url like /lessons/{lesson_id}/comments/
-    @action(methods=['get', 'post'], detail=True, url_path='comments')
+    @action(methods=["get", "post"], detail=True, url_path="comments")
     def get_comments(self, request, pk):
-        
         # POST method: Create comment object
         if request.method.__eq__("POST"):
-            serializer = CommentSerializer(data={
-                'user': request.user.pk,                # user primarykey
-                'lesson': pk,                           # lesson primarykey
-                'content': request.data.get('content')  # content data
-            })
-            serializer.is_valid() # validate comment data
-            
+            serializer = CommentSerializer(
+                data={
+                    "user": request.user.pk,  # user primarykey
+                    "lesson": pk,  # lesson primarykey
+                    "content": request.data.get("content"),  # content data
+                }
+            )
+            serializer.is_valid()  # validate comment data
+
             comment = serializer.save()
-            return Response(CommentSerializer(comment).data, status=rest_framework.status.HTTP_201_CREATED)
-        
-        
+            return Response(
+                CommentSerializer(comment).data,
+                status=rest_framework.status.HTTP_201_CREATED,
+            )
+
         # idk select_related does??!???
-        comment = self.get_object().comment_set.select_related('user').filter(active=True)
-        return Response(CommentSerializer(comment, many=True).data, status=rest_framework.status.HTTP_200_OK)
+        comment = (
+            self.get_object().comment_set.select_related("user").filter(active=True)
+        )
+        return Response(
+            CommentSerializer(comment, many=True).data,
+            status=rest_framework.status.HTTP_200_OK,
+        )
 
 
 class UserViewSet(viewsets.ViewSet, generics.ListAPIView):
     query = User.objects.filter(is_active=True)
     serializer_class = UserSerializer
     parser_classes = [parsers.MultiPartParser]
-    
-    @action(methods=['get', 'patch'], url_path="current-user", detail=False, permission_classes=[permissions.IsAuthenticated])
+
+    @action(
+        methods=["get", "patch"],
+        url_path="current-user",
+        detail=False,
+        permission_classes=[permissions.IsAuthenticated],
+    )
     def get_current_user(self, request):
         user = request.user
-        
-        if request.method.__eq__('PATCH'):
+
+        if request.method.__eq__("PATCH"):
             for key, value in request.data.items():
                 if key in ["first_name", "last_name"]:
-                    setattr(user, key, value) # ~ user.key = value
+                    setattr(user, key, value)  # ~ user.key = value
                 elif key.__eq__("password"):
                     user.set_password(value)
-                
+
             user.save()
-        
+
         return Response(UserSerializer(user).data)
